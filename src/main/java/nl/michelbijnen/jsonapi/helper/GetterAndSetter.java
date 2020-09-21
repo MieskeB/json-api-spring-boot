fa1b77ab7a8bd123629c2679c401670f5302fea3
Michel Bijnen <michelbijnenmichelbijnen@gmail.com>
2020-09-21 16:42:08 +0200

    fix(GetterAndSetter): removed redundant code

--- src/main/java/nl/michelbijnen/jsonapi/helper/GetterAndSetter.java
+++ src/main/java/nl/michelbijnen/jsonapi/helper/GetterAndSetter.java
@@ -3,12 +3,6 @@ package nl.michelbijnen.jsonapi.helper;
 import java.beans.PropertyDescriptor;
 
 public class GetterAndSetter {
-    public void callSetter(Object obj, String fieldName, Object value) throws Exception {
-        PropertyDescriptor pd;
-        pd = new PropertyDescriptor(fieldName, obj.getClass());
-        pd.getWriteMethod().invoke(obj, value);
-    }
-
     public Object callGetter(Object obj, String fieldName) throws Exception {
         PropertyDescriptor pd;
         pd = new PropertyDescriptor(fieldName, obj.getClass());
