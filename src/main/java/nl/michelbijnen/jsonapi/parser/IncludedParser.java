6935595e341efc9fedd1bc26573908a4c1613d1a
Sander de Boer <sander.de.boer4798@hotmail.com>
2021-01-26 21:11:47 +0100

    feat: basic flow works, still a bit buggy

--- src/main/java/nl/michelbijnen/jsonapi/parser/IncludedParser.java
+++ src/main/java/nl/michelbijnen/jsonapi/parser/IncludedParser.java
@@ -1,6 +1,7 @@
 package nl.michelbijnen.jsonapi.parser;
 
-import nl.michelbijnen.jsonapi.annotation.*;
+import nl.michelbijnen.jsonapi.annotation.JsonApiId;
+import nl.michelbijnen.jsonapi.annotation.JsonApiRelation;
 import nl.michelbijnen.jsonapi.helper.GetterAndSetter;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,32 +18,83 @@ class IncludedParser {
         this.linksParser = new LinksParser();
     }
 
-    JSONArray parse(Object object) {
-        JSONArray include = new JSONArray();
+    JSONArray parse(Object object, int maxDepth) {
+        return this.parse(object, new JSONArray(), maxDepth, 0);
+    }
 
-        for (Field relationField : object.getClass().getDeclaredFields()) {
-            if (relationField.isAnnotationPresent(JsonApiRelation.class)) {
-                Object relationObject = GetterAndSetter.callGetter(object, relationField.getName());
-
-                if (relationObject == null) {
-                    continue;
-                }
-
-                if (this.isList(relationObject)) {
-                    for (Object relationObjectSingle : (Collection<Object>) relationObject) {
-                        JSONObject singleIncludeObject = this.dataParser.parse(relationObjectSingle);
-                        singleIncludeObject.put("links", this.linksParser.parse(relationObjectSingle));
-                        include.put(singleIncludeObject);
-                    }
-                } else {
-                    JSONObject singleIncludeObject = this.dataParser.parse(relationObject);
-                    singleIncludeObject.put("links", this.linksParser.parse(relationObject));
-                    include.put(singleIncludeObject);
-                }
-            }
+    JSONArray parse(Object object, JSONArray includeRoot, int maxDepth, int currentDepth) {
+        if (currentDepth > maxDepth && maxDepth != 0) {
+            return includeRoot;
         }
 
-        return include;
+        for (Field relationField : object.getClass().getDeclaredFields()) {
+            if (!relationField.isAnnotationPresent(JsonApiRelation.class)) {
+                continue;
+            }
+
+            Object childRelationObject = GetterAndSetter.callGetter(object, relationField.getName());
+            if (childRelationObject == null) {
+                continue;
+            }
+
+            boolean rootElementExists = this.rootElementExists(includeRoot, childRelationObject);
+
+            if (rootElementExists) {
+                return includeRoot;
+            }
+
+            this.addRelationWithChildrenToIncludeRoot(includeRoot, maxDepth, currentDepth, childRelationObject);
+        }
+
+        return includeRoot;
+    }
+
+    private void addRelationWithChildrenToIncludeRoot(JSONArray includeRoot, int maxDepth, int currentDepth, Object relationObject) {
+        this.addRelationObjectToIncludeRoot(includeRoot, maxDepth, currentDepth, relationObject);
+        int newDepth = maxDepth != 0 ? currentDepth + 1 : 0;
+        this.parse(relationObject, includeRoot, maxDepth, newDepth);
+    }
+
+    private void addRelationObjectToIncludeRoot(JSONArray includeRoot, int maxDepth, int currentDepth, Object relationObject) {
+        if (this.isList(relationObject)) {
+            for (Object relationObjectSingle : (Collection<Object>) relationObject) {
+                JSONObject singleIncludeObject = this.dataParser.parse(relationObjectSingle);
+                singleIncludeObject.put("links", this.linksParser.parse(relationObjectSingle));
+                includeRoot.put(singleIncludeObject);
+                int newDepth = maxDepth != 0 ? currentDepth + 1 : 0;
+                this.parse(relationObjectSingle, includeRoot, maxDepth, newDepth);
+            }
+        } else {
+            JSONObject singleIncludeObject = this.dataParser.parse(relationObject);
+            singleIncludeObject.put("links", this.linksParser.parse(relationObject));
+            includeRoot.put(singleIncludeObject);
+        }
+    }
+
+    private boolean rootElementExists(JSONArray includeRoot, Object relationObject) {
+        for (Field insideRelationField : relationObject.getClass().getDeclaredFields()) {
+            if (!insideRelationField.isAnnotationPresent(JsonApiId.class)) {
+                continue;
+            }
+
+            String id = String.valueOf(GetterAndSetter.callGetter(relationObject, insideRelationField.getName()));
+            boolean rootElementExists = idInRoot(includeRoot, id);
+
+            if (rootElementExists) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean idInRoot(JSONArray includeRoot, String id) {
+        for (int i = 0; i < includeRoot.length(); i++) {
+            JSONObject rootObjectInclude = includeRoot.getJSONObject(i);
+            if (rootObjectInclude.getString("id").equals(id)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private boolean isList(Object object) {
--- src/main/java/nl/michelbijnen/jsonapi/parser/JsonApiConverter.java
+++ src/main/java/nl/michelbijnen/jsonapi/parser/JsonApiConverter.java
@@ -8,8 +8,13 @@ public class JsonApiConverter {
     }
 
     public static String convert(Object object) {
+        int depth = Integer.parseInt(System.getProperty("jsonapi.depth", "1"));
+        return convert(object, depth);
+    }
+
+    public static String convert(Object object, int depth){
         JsonApiParser jsonApiParser = new JsonApiParser();
-        JSONObject result = jsonApiParser.parse(object);
+        JSONObject result = jsonApiParser.parse(object, depth);
         return result.toString();
     }
 }
