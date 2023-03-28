944a619ae5b54594015f22d25116851226c7fe42
MieskeB <m.bijnen@student.tue.nl>
2023-03-28 23:15:23 +0200

    test: added test for linksParser

--- src/test/java/nl/michelbijnen/jsonapi/test/LinkTest.java
+++ src/test/java/nl/michelbijnen/jsonapi/test/LinkTest.java
@@ -1,9 +1,12 @@
 package nl.michelbijnen.jsonapi.test;
 
+import nl.michelbijnen.jsonapi.annotation.JsonApiObject;
 import nl.michelbijnen.jsonapi.parser.JsonApiConverter;
 import nl.michelbijnen.jsonapi.test.mock.MockDataGenerator;
 import nl.michelbijnen.jsonapi.test.mock.ObjectDto;
 import nl.michelbijnen.jsonapi.test.mock.UserDto;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -11,17 +14,18 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.net.MalformedURLException;
 
 public class LinkTest {
 
     private ObjectDto objectDto;
-    private UserDto userDto;
 
     @Before
     public void before() throws CloneNotSupportedException {
         MockDataGenerator generator = MockDataGenerator.getInstance();
         this.objectDto = (ObjectDto) generator.getObjectDto().clone();
-        this.userDto = (UserDto) generator.getUserDto().clone();
     }
 
     @Test
@@ -34,33 +38,51 @@ public class LinkTest {
     @Ignore("Planned for future update")
     public void testIfFirstRelWorks() {
         JSONObject jsonObject = new JSONObject(JsonApiConverter.convert(objectDto));
-        assertEquals("http://localhost:8080/object/" + this.objectDto.getId() + "?page=0", jsonObject.getJSONObject("links").getString("first"));
+        assertEquals("http://localhost:8080/object/" + this.objectDto.getId() + "?page=0",
+                jsonObject.getJSONObject("links").getString("first"));
     }
 
     @Test
     @Ignore("Planned for future update")
     public void testIfPreviousRelWorks() {
         JSONObject jsonObject = new JSONObject(JsonApiConverter.convert(objectDto));
-        assertEquals("http://localhost:8080/object/" + this.objectDto.getId() + "?page=0", jsonObject.getJSONObject("links").getString("previous"));
+        assertEquals("http://localhost:8080/object/" + this.objectDto.getId() + "?page=0",
+                jsonObject.getJSONObject("links").getString("previous"));
     }
 
     @Test
     public void testIfSelfRelWorks() {
         JSONObject jsonObject = new JSONObject(JsonApiConverter.convert(objectDto));
-        assertEquals("http://localhost:8080/object/" + this.objectDto.getId(), jsonObject.getJSONObject("links").getString("self"));
+        assertEquals("http://localhost:8080/object/" + this.objectDto.getId(),
+                jsonObject.getJSONObject("links").getString("self"));
+    }
+
+    @Test
+    public void testIfMalformedUrlThrowsException() {
+        String tempUrl = this.objectDto.getSelfRel();
+        this.objectDto.setSelfRel("NotValidUrl");
+        JSONObject jsonObject = new JSONObject(JsonApiConverter.convert(this.objectDto));
+        this.objectDto.setSelfRel(tempUrl);
+        try {
+            jsonObject.getJSONObject("links");
+            jsonObject.getJSONObject("links").getString("self");
+        } catch (JSONException e) {
+        }
     }
 
     @Test
     @Ignore("Planned for future update")
     public void testIfNextRelWorks() {
         JSONObject jsonObject = new JSONObject(JsonApiConverter.convert(objectDto));
-        assertEquals("http://localhost:8080/object/" + this.objectDto.getId() + "?page=2", jsonObject.getJSONObject("links").getString("next"));
+        assertEquals("http://localhost:8080/object/" + this.objectDto.getId() + "?page=2",
+                jsonObject.getJSONObject("links").getString("next"));
     }
 
     @Test
     @Ignore("Planned for future update")
     public void testIfLastRelWorks() {
         JSONObject jsonObject = new JSONObject(JsonApiConverter.convert(objectDto));
-        assertEquals("http://localhost:8080/object/" + this.objectDto.getId() + "?page=2", jsonObject.getJSONObject("links").getString("last"));
+        assertEquals("http://localhost:8080/object/" + this.objectDto.getId() + "?page=2",
+                jsonObject.getJSONObject("links").getString("last"));
     }
 }
